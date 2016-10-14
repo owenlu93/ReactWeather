a59461dbbd2e5137fa22a7073ecfbc17d9a9623e
Owen Lu <owen_lu@live.com>
2016-10-14 15:42:29 -0700

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ var express = require('express');
 
 // Create our applciation
 var app = express();
+// environment variable
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req,res,next) {
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  }else{
+    res.redirect('http://' req.hostname + req.url);
+  }
+});
 
 // Add functionality to app
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-    console.log('Express server is up on port 3000');
-})
+app.listen(PORT, function() {
+    console.log('Express server is up on port ' + PORT);
+});
